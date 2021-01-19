7becaa7b43f5fae4101c8739ed96ebf34bab2b50
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-01-19 14:18:43 -0300

    Changed API URL to the server and changed Android Default Color

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // Dev:http://10.0.2.2:3333
   // Prod: https://nodedeploy.leonardoribeiro.com
-  baseURL: 'http://10.0.2.2:3333',
+  baseURL: 'https://nodedeploy.leonardoribeiro.com',
 });
 
 export default api;
